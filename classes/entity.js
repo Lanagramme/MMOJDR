7250744cd5c7d0a050742e594052dde97eef55a8
Ehacko <Darkdav.T@gmail.com>
2021-04-28 18:28:36 -0400

    init classes

---
+++ classes/entity.js
@@ -0,0 +1,16 @@
+class Entity {  
+  constructor(id){
+    this.id
+    this.x = 1500
+    this.y = 1600
+    this.spdX = 0
+    this.spdY = 0
+  }
+  update(){ this.updatePosition() }
+  updatePosition(){
+    this.x += this.spdX
+    this.y += this.spdY
+  }
+}
+
+module.exports = { Entity }
