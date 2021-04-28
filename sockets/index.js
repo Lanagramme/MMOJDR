6a6c23419dc591b3b69e870abf0aa96c2c263d35
Ehacko <Darkdav.T@gmail.com>
2021-04-28 18:05:17 -0400

    add socket io server side script

--- sockets/index.js
+++ sockets/index.js
@@ -1,5 +1,55 @@
 module.exports = function (io) {
-  io.on('connection', function(socket){
-    console.log('A user connected');
-  });
+  
+var socket_list = {}
+io.sockets.on('connection', (socket) => {  
+  socket.id = Math.random()
+  socket_list[socket.id] = socket
+
+  console.log('Nouvelle connection :: ' + socket.id)
+  Player.onConnect(socket)
+  
+  socket.on('disconnect',() => {
+    delete socket_list[socket.id]
+    Player.onDisconnect (socket)
+    console.log(socket.id + " :: déconnecté")
+  })
+  
+})
+setInterval(()=>{
+  pack = Player.update()
+  
+  for (i in socket_list) {
+    let socket = socket_list[i]
+    for (j of pack){
+      if (j.number == Player.list[i].number) j.color = "red"
+      else j.color = "blue"
+    }
+    socket.emit('newPositions', pack)
+  }
+}, 1000/25)
+
+// collision tests
+/*
+function testCollisionBetweenPoints(A,B){
+  var vx = A.x - B.x
+  var vy = A.y - B.y
+  distance = Math.sqrt(vx*vx+vy*vy)
+  return distance < 10
+}
+
+rect = {
+  height: 30,
+  width:30,
+  x: Entity.x-this.width/2,
+  y: Entity.y-this.height/2,
+}
+
+function testCollisionBetweenEntities(A,B){
+  return A.x <= B.x + B.width
+      && B.x <= A.x + A.width
+      && A.y <= B.y + B.height
+      && B.y <= A.y + A.height
+}
+*/
+
 }
