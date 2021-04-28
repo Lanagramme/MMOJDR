64af82afa162bb197a1ba31d02f977e1649f4332
Lanagramme <46745901+Lanagramme@users.noreply.github.com>
2021-04-28 17:47:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,98 +1,20 @@
-// const { rejects } = require('assert')
 var express = require('express')
-var app = express()
+var app  = express()
+var io   = require('socket.io')(serv,{})
 var serv = require('http').Server(app)
-cl = console.log
+
+const cl = console.log
 const { Player } = require('./server/classes/player.js')
+// const { rejects } = require('assert')
 
-// if the reqest does not content a path, return  index ex: domain:2000
-app.get('/',(req, res) => {
-  res.sendFile(__dirname + '/client/index.html')
-})
-
-// if a specific path is required inside the client directory, send the corresponding file ex: domain:2000/client/file
+// /client/index.html is the default landing
+// no file from the server can be accepted except those from /client
+app.get('/', (req, res) => { res.sendFile(__dirname + '/client/index.html') })
 app.use('/client', express.static(__dirname + '/client'))
-
 serv.listen(1616)
 
 var socket_list = {}
-
-/* class Entity {
-  constructor(id){
-    this.id
-    this.x = 1500
-    this.y = 1600
-    this.spdX = 0
-    this.spdY = 0
-  }
-  update(){ this.updatePosition() }
-  updatePosition(){
-    this.x += this.spdX
-    this.y += this.spdY
-  }
-}
-
-class Player extends Entity{
-  constructor(id,x,y){
-    super(id,x,y)
-    this.pressingUp = false
-    this.pressingDown = false
-    this.pressingLeft = false
-    this.pressingRight = false
-    this.number = Math.floor(Math.random() * 100)
-    this.maxSpeed = 10
-
-    Player.list[id] = this
-  }
-
-  update(){
-    this.updateSpeed()
-    super.update()
-  }
-  
-  updateSpeed(){
-    if( this.pressingUp ) this.spdY = -this.maxSpeed
-    else if( this.pressingDown ) this.spdY =  this.maxSpeed
-    else this.spdY = 0
-    if( this.pressingRight ) this.spdX =  this.maxSpeed
-    else if( this.pressingLeft ) this.spdX = -this.maxSpeed
-    else this.spdX = 0
-  }
-}
-Player.list = {}
-Player.onConnect = (socket) => {
-  var player = new Player(socket.id)
-  socket.on('keypress', (data)=> {
-    if(data.inputId == 'up'   ) player.pressingUp    = data.state
-    else if(data.inputId == 'down' ) player.pressingDown  = data.state
-    else if(data.inputId == 'left' ) player.pressingLeft  = data.state
-    else if(data.inputId == 'right') player.pressingRight = data.state
-   })
-
-   socket.emit('init', { id:socket.id })
-}
-Player.onDisconnect = (socket) => {
-  delete Player.list[socket.id]
-}
-Player.update = () =>{
-  // liste mise à jour des joueurs
-  var pack = []
-  for (i in Player.list) {
-    player = Player.list[i]
-    player.update()
-    pack.push({
-      x : player.x,
-      y : player.y,
-      number : player.number
-    })
-  }
-  return pack
-}
-*/
-
-var io = require('socket.io')(serv,{})
-io.sockets.on('connection', (socket) => {
-  // connection
+io.sockets.on('connection', (socket) => {  
   socket.id = Math.random()
   socket_list[socket.id] = socket
 
